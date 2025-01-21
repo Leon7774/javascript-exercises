1b9531c4e35eb8c73dbd6e8eb9978be06f1d9128
Leon <galileon.destura@gmail.com>
2025-01-21 19:52:02 +0800

    Finished exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if(typeof tempF != 'number'){
+    return "ERROR";
+  }
+
+  let returnVal = 5/9 * (tempF - 32);
+
+  return (returnVal % 1 == 0) ? returnVal : parseFloat(returnVal.toFixed(1)) 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if(typeof tempC != 'number'){
+    return "ERROR";
+  }
+
+  let returnVal = (tempC * 9/5) + 32;
+
+  return (returnVal % 1 == 0) ? returnVal : parseFloat(returnVal.toFixed(1))
 };
 
 // Do not edit below this line
